d2fc2d0c0bd890a9c5178050e30ce22adb963868
HudaAlaaraji <huda.isc@gmail.com>
2022-12-25 15:58:28 +0000

    Saved progress on GreaterOrLesser 2022-12-25 15:58:28 +0000

--- GreaterOrLesser/src/main/java/GreaterOrLesser.java
+++ GreaterOrLesser/src/main/java/GreaterOrLesser.java
@@ -9,7 +9,17 @@ public class GreaterOrLesser {
      * @return  If a is less than b, return the string "Lesser". if a is greater than b, return "Greater".
      *          If neither is true, return "Equal". This problem is case-sensitive!
      */
-    public String decide(int a, int b){
-        return "Equal";
+    public static void main(String[] args) {
+        
+
+        if (a < b) {
+            System.out.println("Lesser");
+        } else if (a > b) {
+            System.out.println("Greater");
+        } else {
+            System.out.println("Equal");
+        }
     }
 }
+
+    
