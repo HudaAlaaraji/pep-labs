c882793994b7bc0a7bcfe9c45396b9cdcda86b27
HudaAlaaraji <huda.isc@gmail.com>
2022-12-29 00:57:31 +0000

    Saved progress on IntroToOOP 2022-12-29 00:57:31 +0000

--- IntroToOOP/src/main/java/Bird.java
+++ IntroToOOP/src/main/java/Bird.java
@@ -46,7 +46,7 @@ public class Bird {
      *  * greater than or equal to 3.
      */
     public String Sound(){
-        if (ageMonths< 3){
+        if (ageMonths < 3){
         return "Tweet";
     }
     else{
