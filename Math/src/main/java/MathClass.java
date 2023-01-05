df508bf5d0159064af0c73e0f8f1fc010c680e57
HudaAlaaraji <huda.isc@gmail.com>
2023-01-05 00:34:08 +0000

    Saved progress on EvenOrOdd 2023-01-05 00:34:08 +0000

--- Math/src/main/java/MathClass.java
+++ Math/src/main/java/MathClass.java
@@ -19,7 +19,7 @@ public class MathClass {
      *
      * @return return a^b using the pow() method of the Math class.
      */
-    public double mathPow(double a, double b){
+    public double mathPow(double a, double b) {
         System.out.println(Math.pow(a,b));
     }
 }
