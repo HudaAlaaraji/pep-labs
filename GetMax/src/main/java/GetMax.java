7e8c25f3fc561468d857946b8228a859ba9f7005
HudaAlaaraji <huda.isc@gmail.com>
2023-01-10 01:49:29 +0000

    Saved progress on GetMax 2023-01-10 01:49:29 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int maxVal = -901;
+        for(int i = 0; 1<arr.length; i++){
+            if (maxVal < arr[i]){
+               maxVal = arr[i];
+            }
+        }
+        return maxVal;
     }
 }
