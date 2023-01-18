794e292dc3fd0373ae59261553b310ab10108589
HudaAlaaraji <huda.isc@gmail.com>
2023-01-18 01:56:16 +0000

    Saved progress on Contains 2023-01-18 01:56:16 +0000

--- Contains/src/main/java/Contains.java
+++ Contains/src/main/java/Contains.java
@@ -10,7 +10,13 @@ public class Contains {
      * @param target the value that the method should be searching for.
      * @return true if arr contains target, false otherwise.
      */
-    public boolean arrayContains(int[] arr, int target){
-        return false;
+    public boolean arrayContains(int[ ] arr, int target){
+        for (int element : arr){
+            if (element == target){
+                return true;
+            } 
+            return false;
     }
 }
+}
+  
