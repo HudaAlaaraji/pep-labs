8578ec40bd71ea83731cdf78580ee81b345bcf41
HudaAlaaraji <huda.isc@gmail.com>
2023-01-18 15:32:33 +0000

    Saved progress on Lists 2023-01-18 15:32:33 +0000

--- BankAccount/test-output.sh
+++ BankAccount/test-output.sh
@@ -4,8 +4,3 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running BankServiceTest
-
-Results :
-
-Tests run: 0, Failures: 0, Errors: 0, Skipped: 0
-
