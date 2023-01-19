b6145a7fdf82e5993df2eea05c0e0ef9304ced3a
HudaAlaaraji <huda.isc@gmail.com>
2023-01-19 16:59:46 +0000

    Saved progress on OrderBy 2023-01-19 16:59:46 +0000

--- OrderBy/problem1.sql
+++ OrderBy/problem1.sql
@@ -1,6 +1,1 @@
-SELECT * FROM charactertable ORDER BY lastname ASC;
-CREATE TABLE character (
- *      id SERIAL PRIMARY KEY,
- *      first_name VARCHAR(255),
- *      last_name VARCHAR(255)
- * );
+SELECT * FROM character ORDER BY last_name ASC thencomparing (first_name);
