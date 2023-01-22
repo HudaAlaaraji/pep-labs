802a62e384d821f5ce5304bb9e1ae85ecaf951b1
HudaAlaaraji <huda.isc@gmail.com>
2023-01-22 02:15:05 +0000

    Saved progress on StatusCodes 2023-01-22 02:15:05 +0000

--- StatusCodes/src/main/java/com/revature/JavalinSingleton.java
+++ StatusCodes/src/main/java/com/revature/JavalinSingleton.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import com.azul.crs.client.service.ClientService;
+
 import io.javalin.Javalin;
 
 public class JavalinSingleton {
@@ -14,8 +16,9 @@ public class JavalinSingleton {
          */
         app.get("/client-side-err", ctx -> {
         
-            //write logic here
-
+            String client-side-er = ctx.pathParam(arg0: "name");
+            Client client = ClientService.getclientbyname(client-side-err);
+            ctx,json(client);
         });
 
         return app;
