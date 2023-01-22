c687ebc81f412f4a24deee8bda525371b3eae74f
HudaAlaaraji <huda.isc@gmail.com>
2023-01-22 15:23:22 +0000

    Saved progress on IndexOf 2023-01-22 15:23:22 +0000

--- IndexOf/src/main/java/IndexOf.java
+++ IndexOf/src/main/java/IndexOf.java
@@ -12,7 +12,7 @@ public class IndexOf {
      */
     public int getIndex(int[] arr, int n){
         for(int i =0; i < arr.length; i++) 
-        if(arr[2]= 7){ 
+        if(arr[i]= ?){ 
             return 7; 
         }
         else {
