830b3f62ebfb77a87ef3f4470709ac01011cc323
HudaAlaaraji <huda.isc@gmail.com>
2023-01-23 18:14:13 +0000

    Saved progress on Contains 2023-01-23 18:14:13 +0000

--- Contains/src/main/java/Contains.java
+++ Contains/src/main/java/Contains.java
@@ -11,8 +11,8 @@ public class Contains {
      * @return true if arr contains target, false otherwise.
      */
     public boolean arrayContains(int[ ] arr, int target){
-        for(int i = 0; i< target; i++)
-            if( i == target){
+        for(int i = 0; i< arr.length; i++)
+            if(arr[i] == target){
                 return true;
             } 
             return false;
