c8fe7f12caf9f9e3cb9523c109e29b89fcfd62e6
HudaAlaaraji <huda.isc@gmail.com>
2023-01-24 04:45:39 +0000

    Saved progress on ReturnLongestString 2023-01-24 04:45:39 +0000

--- IndexOf/src/main/java/IndexOf.java
+++ IndexOf/src/main/java/IndexOf.java
@@ -11,12 +11,13 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
-        for(int i =0; i < arr.length; i++) 
-        if(arr[i]= ?){ 
-            return 7; 
+        for(int i =0; i < arr.length; i++){
+            if(arr[i] == n){
+        return i;
+            }
         }
-        else {
-            return -1;
+        return -1;
     }
-    }
-    }
+}
+
+
