2cace0168c0aef6860c48a41951792729a910d2e
HudaAlaaraji <huda.isc@gmail.com>
2023-01-27 23:29:35 +0000

    Saved progress on CombineArrays 2023-01-27 23:29:35 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,7 +8,7 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int[] arr = arr1 + arr2;
+        return arr;
     }
 }
