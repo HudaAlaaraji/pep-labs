2b6d1dfe00d04840808463d2ed7f559b382ee51c
HudaAlaaraji <huda.isc@gmail.com>
2023-01-31 17:27:40 +0000

    Saved progress on Pyramid 2023-01-31 17:27:40 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,15 +8,19 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-        int[] concatenateInt = combineInt(arr1, arr2);
-        System.out.println("concatenated Int array : " + Arrays.toString(concatenateInt));
+       
     }
-}
-
-
-
- //int[]a = {0};
-//int[]b = {0};
-//int[]c = c.concat(a+b);
-//for(int i=0; i<c.length; i++)
-//System.out.print(c[i]+" ");
+      
+        //int[]c = new int[arr1.length+arr2.length];
+        //int count = 0;
+        //for(int i = 0; i<arr1.length; i++) {
+          // c[i] = arr1[i];
+           //count++;
+        //}
+        //for(int j = 0;j<arr2.length;j++) {
+           //c[count++] = arr2[j];
+        //}
+        //for(int i = 0;i<c.length;i++)
+        //System.out.print(c[i]+" ");
+     //}
+  //}
