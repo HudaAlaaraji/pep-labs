f370a7d3c3bd6ed48df1b8ef13a255100a0d95da
HudaAlaaraji <huda.isc@gmail.com>
2023-01-31 17:30:40 +0000

    Saved progress on CombineArrays 2023-01-31 17:30:40 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,19 +8,16 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-       
-    }
-      
-        //int[]c = new int[arr1.length+arr2.length];
-        //int count = 0;
-        //for(int i = 0; i<arr1.length; i++) {
-          // c[i] = arr1[i];
-           //count++;
-        //}
-        //for(int j = 0;j<arr2.length;j++) {
-           //c[count++] = arr2[j];
-        //}
-        //for(int i = 0;i<c.length;i++)
-        //System.out.print(c[i]+" ");
-     //}
-  //}
+        int[]c = new int[arr1.length+arr2.length];
+        int count = 0;
+        for(int i = 0; i<arr1.length; i++) {
+           c[i] = arr1[i];
+           count++;
+        }
+        for(int j = 0;j<arr2.length;j++) {
+           c[count++] = arr2[j];
+        }
+        for(int i = 0;i<c.length;i++)
+        System.out.print(c[i]+" ");
+     }
+  }
