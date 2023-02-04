3b574230d8030c54a42ed1c567ab5b9344e72e2d
HudaAlaaraji <huda.isc@gmail.com>
2023-02-04 04:39:01 +0000

    Saved progress on WordCount 2023-02-04 04:39:01 +0000

--- WordCount/src/main/java/ReturnWordCount.java
+++ WordCount/src/main/java/ReturnWordCount.java
@@ -5,6 +5,8 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
-    }
+        String actual = in.count(in);
+        return in.count(in);
+
+        }
 }
