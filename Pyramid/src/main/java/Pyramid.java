2ba8987a1f8b71e6cf332704d72f3199c2ab0cd8
HudaAlaaraji <huda.isc@gmail.com>
2023-02-08 02:54:12 +0000

    Saved progress on Pyramid 2023-02-08 02:54:12 +0000

--- Pyramid/src/main/java/Pyramid.java
+++ Pyramid/src/main/java/Pyramid.java
@@ -30,22 +30,16 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        Scanner in = new Scanner(System.in);
-        int r = 0, i, j;
-        System.out.println("n");
-        r = in.nextInt();
-        System.out.print("\n");
-        for (i = 1; i <= r; ++i) {
-            for (j = 1; j <= i; ++j)
-                System.out.println("*");
-            System.out.print("\n");
+        int rows = n;
+        for (int i = 1; i <= rows; ++i) {
+          for (int j = 1; j <= i; ++j) {
+            System.out.print("* ");
+          }
+          System.out.println();
         }
+      }
     }
-}
-    //public static void main(String args[])
     
-        //int k = r;
-        //returnPyramid(k);
     
 
 
